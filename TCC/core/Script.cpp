914a2ce8a77a85b0f5669d5237fcb1743e001822
suisen.no.ryuu <suisen.no.ryuu@17fd7b7e-20b4-11de-a108-cd2f117ce590>
2009-04-23 02:40:12 +0000

    Documented the files in the "core" folder Improved EventListener (now updated to handle GUI Events)

--- TCC/core/Script.cpp
+++ TCC/core/Script.cpp
@@ -26,49 +26,41 @@ void Script::endScript(){
   lua_close(state);
 }
 
-int Script::getIntScript(char *name)
-{
+/*
+ * Recover simple values processed by LUA
+ */
+
+int Script::getIntScript(char *name){
   lua_getglobal(state, name);
   return (int)lua_tonumber(state, -1);
 }
 
-double Script::getDoubleScript(char *name)
-{
+double Script::getDoubleScript(char *name){
   lua_getglobal(state, name);
   return (double)lua_tonumber(state, -1);
 }
 
-bool Script::getBoolScript(char *name)
-{
+bool Script::getBoolScript(char *name){
   lua_getglobal(state, name);
   return lua_toboolean(state, -1);
-
 }
 
-const char * Script::getStringScript(char *name)
-{
+const char * Script::getStringScript(char *name){
   lua_getglobal(state, name);
   return lua_tostring(state, -1);
 }
 
+/*
+ * Recover struct values processed by LUA
+ * The parameter name is the name of the structure to be searched
+ */
 
-//
-//Função openStruct
-//Objetivo: abre uma estrutura composta
-//Parâmetros: state, estado do lua, filename, nome da estrutura
-//
-void Script::openStruct(char *name)
-{
+
+void Script::openStruct(char *name){
   lua_getglobal(state, name); 
 }
 
-//
-//Funções get????Struct
-//Objetivo: pegam valores de dentro de uma estrutura composta
-//Parâmetros: state, estado do lua, name, nome do campo
-//
-char* Script::getStringStruct(char *name)
-{
+char* Script::getStringStruct(char *name){
   char aux[255];
   lua_pushstring(state, name);
   lua_gettable(state, -2);
@@ -78,8 +70,7 @@ char* Script::getStringStruct(char *name)
   return aux;
 }
 
-int Script::getIntStruct(char *name)
-{
+int Script::getIntStruct(char *name){
   int aux;
   lua_pushstring(state, name);
   lua_gettable(state, -2);
@@ -89,8 +80,7 @@ int Script::getIntStruct(char *name)
   return aux;
 }
 
-double Script::getDoubleStruct(char *name)
-{
+double Script::getDoubleStruct(char *name){
   double aux;
   lua_pushstring(state, name);
   lua_gettable(state, -2);
@@ -100,45 +90,46 @@ double Script::getDoubleStruct(char *name)
   return aux;
 }
 
-//
-//Função openSubScript
-//Objetivo: abre uma sub estrutura (estrutura dentro de estrutura)
-//Parâmetros: state, estado do lua, name, nome da sub estrutura
-//
-void Script::openSubStruct(char *name)
-{
+
+/*
+ * Opens a structure inside an other structure
+ */
+void Script::openSubStruct(char *name){
   lua_pushstring(state, name);
   lua_gettable(state, -2);
 }
 
-//
-//Função closeSubStruct
-//Objetivo: finaliza sub estrutura
-//Parâmetros: state, estado do lua
-//
-void Script::closeSubStruct()
-{
+/*
+ * Closes a sub-structure
+ */
+void Script::closeSubStruct(){
   lua_pop(state,1);
 }
 
-//
-//Função callFunction
-//Objetivo: chama uma função do script
-//Parâmetros: state, estado do lua
-//
-void Script::callFunction(char* name, int *args, int nArgs , int *res, int nRes)
-{
-  // Busca função
+/*
+ * Opens a structure inside an other structure
+ */
+void Script::callFunction(char* name, int *args, int nArgs , int *res, int nRes){
+  
+  /*
+   * Searchs the function name
+   */
 	lua_getglobal(state, name);
-
-  // Insere argumentos
+  
+  /*
+   * Insert the arguments
+   */
   for (int i = 0; i < nArgs; i++)
 	  lua_pushnumber(state, args[i]);
-
-  // Chama a função
+  
+  /*
+   * call the function
+   */
 	lua_call(state, nArgs, nRes);
-
-	// Recupera os resultados
+	
+  /*
+   * Recover results
+   */
   for (int i = 0; i < nRes; i++)
 	  res[i] = (int)lua_tonumber(state, -(nRes - i));
 
