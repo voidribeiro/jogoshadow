47847cb71210ae8bbe2497f0d2a83a96fa8e896a
rafarlira <rafarlira@17fd7b7e-20b4-11de-a108-cd2f117ce590>
2009-09-22 15:19:23 +0000

    Adding and removing references to the map

--- trunk/TCC/core/TextureManager.cpp
+++ trunk/TCC/core/TextureManager.cpp
@@ -5,11 +5,24 @@ TextureManager::TextureManager(IVideoDriver* driver){
 }
 
 TextureManager::~TextureManager(){
+	for(std::map<std::string, video::ITexture*>::const_iterator it = textureMap.begin(); it != textureMap.end(); ++it){
+		if (it->second != NULL){
+			driver->removeTexture(it->second);
+		}
+	}
 }
 
 video::ITexture* TextureManager::GetTexture(std::string imagePath){
-	return driver->getTexture(imagePath.c_str());
+	if (textureMap[imagePath] == NULL){
+		textureMap[imagePath] = driver->getTexture(imagePath.c_str());
+	}
+	return textureMap[imagePath];
 }
 
 void TextureManager::RemoveTexture(std::string imagePath){
+	if (textureMap[imagePath] != NULL){
+		driver->removeTexture(textureMap[imagePath]);
+		//TODO - Not removing the reference from the map. But the image is removed;
+		textureMap[imagePath] = NULL;
+	}
 }
