6cb2c1e8911afdb75fe819bb5ecea55c9bde3b2c
rafarlira <rafarlira@17fd7b7e-20b4-11de-a108-cd2f117ce590>
2009-10-09 12:29:13 +0000

    Adding methods to implement

--- trunk/TCC/core/GameObject.cpp
+++ trunk/TCC/core/GameObject.cpp
@@ -5,3 +5,26 @@ GameObject::GameObject(){
 
 GameObject::~GameObject(){
 }
+
+//TODO - Implement all of this
+
+void GameObject::AddComponent(AbstractComponent*){
+}
+
+void GameObject::RemoveComponent(int componentType){
+}
+
+void GameObject::RemoveComponentByIndex(int index){
+}
+
+int GameObject::GetComponentsCount(){
+  return 0;
+}
+ 
+AbstractComponent* GameObject::GetComponent(int componentType){
+  return NULL;
+}
+
+AbstractComponent* GameObject::GetComponentByIndex(int index){
+  return NULL;
+}
