43f66a5b0b2dba61243746ceb94035cb70f755d2
rafarlira <rafarlira@17fd7b7e-20b4-11de-a108-cd2f117ce590>
2009-10-16 05:04:17 +0000

    Adding how it should work

--- TCC/Scripts/Start/loader.lua
+++ TCC/Scripts/Start/loader.lua
@@ -1,6 +1,18 @@
+--local splashTime = 3000
+
 function startScript()
   print("Loading Start.")
   local gameObject = GameObject.Instantiate()
+  --Load Splash Image
   local component = ComponentImage.Instantiate(path .. "resources\\splash\\lucasarts_logo.jpg")
   component:AddTo(gameObject)
+  --Set a Timer Function
+  --local component = ComponentTimer.Instantiate(splashTime,path .. "Scripts\\Start\\loader.lua","onTimeOut")
+  --component:AddTo(gameObject)
+end
+
+function onTimeOut()
+  print("It should not get here before call timeout")
+	--local game = Game.GetInstance()
+	--game:LoadLevel("Level1");
 end
