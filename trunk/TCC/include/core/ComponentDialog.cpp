5e0a0d16ee8434bd62e536dbc46c9339a78455d6
suisen.no.ryuu <suisen.no.ryuu@17fd7b7e-20b4-11de-a108-cd2f117ce590>
2010-01-10 16:27:51 +0000

    first experiments with dialog options

--- trunk/TCC/include/core/ComponentDialog.cpp
+++ trunk/TCC/include/core/ComponentDialog.cpp
@@ -13,7 +13,7 @@ rectOptions(64,64,736,100)
   windowText = env->addStaticText(L"", rectText, false, true, windowAll, -1, true);
   windowText->setBackgroundColor(video::SColor(200,250,250,250));
 
-  windowOptions = env->addStaticText(L" Opcoes de dialogo aki", rectOptions, false, true, windowAll, -1, true);
+  windowOptions = env->addStaticText(L"", rectOptions, false, true, windowAll, -1, true);
   windowOptions->setBackgroundColor(video::SColor(200,200,200,200));
 
 }
@@ -35,6 +35,37 @@ void ComponentDialog::Update(){
 
 void ComponentDialog::Say(wchar_t* message){
   windowText->setText(message);
+  clearOptions();
+}
+
+
+void ComponentDialog::AddOption(std::string instanceName, wchar_t* buttonText, int posX, int posY){
+  IrrlichtDevice* device = DeviceManager::GetDevice();
+  IGUIEnvironment* env = device->getGUIEnvironment();
+
+  irr::gui::IGUIElement* buf = env->addButton( core::rect<s32>(posX, posY, 
+                                               posX + 150,
+                                               posY + 25 ), 
+                                               windowOptions, -1, buttonText );
+
+  options[instanceName] = buf;
+
+}
+
+bool ComponentDialog::IsButtonPressed(const std::string instanceName){
+  return ((irr::gui::IGUIButton*)options[instanceName])->isPressed();
+}   
+
+void ComponentDialog::clearOptions(){
+/*  while (options.size != 0){
+    AbstractComponent* c = (*options.begin());
+    options.pop_front();
+    if (c != NULL){
+      delete c;
+      c = NULL;
+    }
+  }
+*/
 }
 
 void ComponentDialog::SetPlayerImage(std::string filename){
@@ -121,6 +152,30 @@ int ComponentDialogBinder::bnd_Say(lua_State* L){
   componentDialog->Say((wchar_t*)message.c_str());
 
   return 1;
+}
+
+int ComponentDialogBinder::bnd_AddOption(lua_State *L){
+  LuaBinder binder(L);
+  ComponentDialog* componentDialog  = (ComponentDialog*) binder.checkusertype(1,"ComponentDialog");
+
+  std::string instanceName  = lua_tostring(L, 2);
+  
+  std::string  buf = lua_tostring(L, 3);
+  std::wstring buttonText    = std::wstring( buf.begin(), buf.end() );
+
+  int posX = lua_tointeger(L, 4);
+  int posY = lua_tointeger(L, 5);
+
+  componentDialog->AddOption(instanceName, (wchar_t*)buttonText.c_str(), posX, posY);
+
+  return 1;
+}
+
+int ComponentDialogBinder::bnd_IsButtonPressed(lua_State* L){
+  LuaBinder binder(L); 
+  ComponentDialog* componentDialog  = (ComponentDialog*) binder.checkusertype(1,"ComponentDialog");
+  binder.pushnumber(componentDialog->IsButtonPressed(lua_tostring(L,2)));
+  return 1;   
 }
 
 int ComponentDialogBinder::bnd_SetPlayerImage(lua_State* L){
