ccd60780957921c7b897cb18755532672931da3e
suisen.no.ryuu <suisen.no.ryuu@17fd7b7e-20b4-11de-a108-cd2f117ce590>
2010-01-10 19:29:14 +0000

    added dialog option response to mouse click

--- TCC/Scripts/Start/loader.lua
+++ TCC/Scripts/Start/loader.lua
@@ -1,4 +1,4 @@
-local splashTime = 10000
+local splashTime = 3000
 
 function startScript()
   print("Loading Start.")
@@ -8,38 +8,16 @@ function startScript()
   local component = ComponentImage.Instantiate(path .. "resources\\splash\\lucasarts_logo.jpg")
   component:AddTo(gameObject)
   
-  local component = ComponentDialog.Instantiate()
-
-  component:Say( " Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.")
-  
-  component:SetPlayerImage(path .. "resources\\hero\\hero.png")
-  component:SetNpcImage(path .. "resources\\hero\\hero.png")
-  component:SetFont(path .. "resources\\font\\fontlucida.png")
-
-  component:AddOption("b1", "Diga Alguma coisa!", 10, 10);
-  
-  component:AddTo(gameObject)
-  
   --Set a Timer Function
 
   local component = ComponentTimer.Instantiate(splashTime, path .. "Scripts\\Start\\loader.lua","onTimeOut")
   component:AddTo(gameObject)
-
---  local component = ComponentTimer.Instantiate(5000, path .. "Scripts\\Start\\loader.lua","sayOther")
---  component:AddTo(gameObject)
 
 end
 
 function onTimeOut()
   print("It should not get here before call timeout")
 	local game = Game.GetInstance()
-	game:LoadLevel("Level2");
+	game:LoadLevel("Level1");
 end
 
-function sayOther()
-
-  local component = ComponentDialog.GetFrom("Logo")
-  component:Say("Era uma vez um outro texto de dialogo, blah blah blah!!!")
-  --component:SetVisible(false)
-
-end
