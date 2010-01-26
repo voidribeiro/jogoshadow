18b80d62d079534f655121267281d491bb673f62
suisen.no.ryuu <suisen.no.ryuu@17fd7b7e-20b4-11de-a108-cd2f117ce590>
2010-01-26 23:43:34 +0000

    improved the Item class

--- TCC/game/Item.cpp
+++ TCC/game/Item.cpp
@@ -1,19 +1,27 @@
 #include "Item.h"
 
 Item::Item(){
-
 }
 
-Item::Item(string _name, string _description):name(_name){
-  description[0] = _description;
+Item::Item(string _name):name(_name){
 }
 
 Item::~Item(){
-  if (node)
-    node->remove();
-  node = 0;
+}
 
-  if (mesh)
-    mesh->drop();
-	mesh = 0;
+void Item::addDescription(const string description){
+  descriptionList.push_back(description);
+}
+
+string Item::getDescription(const int idx){
+  list<string>::iterator it;
+  it = descriptionList.begin();
+
+  if(descriptionList.size() < idx)
+    it = descriptionList.end();
+  else
+    for(int i =0 ; i < idx; i++)
+      it++;
+
+  return (*it);
 }
