aa4c021fcdd9fa0363097a8fd5e4d347c07f037f
suisen.no.ryuu <suisen.no.ryuu@17fd7b7e-20b4-11de-a108-cd2f117ce590>
2010-02-07 02:07:42 +0000

    improved Inventory Class

--- trunk/TCC/include/game/Inventory.cpp
+++ trunk/TCC/include/game/Inventory.cpp
@@ -25,3 +25,36 @@ void Inventory::Remove(std::string objName){
 GameObject* Inventory::getGameObject(std::string objName){
   return items[objName];
 }
+
+void Inventory::SetBackground(std::string filename){
+  backgroundImage = TextureManager::GetTexture(filename);
+}
+
+void Inventory::setVisible(bool visible){
+  isVisible = visible;
+}
+
+void Inventory::Draw(){
+  if (!isVisible)
+    return;
+  irr::video::IVideoDriver* driver = DeviceManager::GetDriver();
+  driver->draw2DImage(backgroundImage, core::position2d<s32>(0,0));
+}
+
+
+ComponentModel* Inventory::getComponentModel(std::string objName){
+  ComponentModel* cModel = (ComponentModel*)items[objName]->GetComponent(CMODEL);
+  if (cModel == NULL)
+    printf("Missing Model Reference\n");
+
+  return cModel;
+}
+
+
+ComponentImage* Inventory::getComponentImage(std::string objName){
+  ComponentImage* cImage = (ComponentImage*)items[objName]->GetComponent(CIMAGE);
+  if (cImage == NULL)
+    printf("Missing Model Reference\n");
+
+  return cImage;
+}
