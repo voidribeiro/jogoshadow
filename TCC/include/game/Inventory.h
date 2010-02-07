c6340d21e7f1641c53cac653cab6af4cc0bdea98
suisen.no.ryuu <suisen.no.ryuu@17fd7b7e-20b4-11de-a108-cd2f117ce590>
2010-02-07 02:07:42 +0000

    improved Inventory Class

--- TCC/include/game/Inventory.h
+++ TCC/include/game/Inventory.h
@@ -2,12 +2,19 @@
 #define _INVENTORY_H_
 
 #include "GameObject.h"
+#include "TextureManager.h"
+#include "ComponentModel.h"
+#include "ComponentImage.h"
 #include <map>
 
 class Inventory {
   private:
     std::map <std::string, GameObject*> items;
 
+    core::rect<s32> rectAll;   //rect da janela toda
+    ITexture* backgroundImage; //imagem de fundo
+    bool isVisible;            //se o inventário está visível, caso verdadeiro deve mudar o input para o contexto 
+                               //de inventário e não interagir na tela "atrás"
   public:
     explicit Inventory();
     virtual ~Inventory();
@@ -15,7 +22,13 @@ class Inventory {
     void Add(std::string objName, GameObject* obj);     //adiciona o ítem ao jogador
     void Remove(std::string objName);                   //remove o ítem do jogador
 
+    void SetBackground(std::string filename);
+    void setVisible(bool visible);
+    void Draw();
+
     GameObject* getGameObject(std::string objName);
+    ComponentModel* getComponentModel(std::string objName);
+    ComponentImage* getComponentImage(std::string objName);
 
 };
 
