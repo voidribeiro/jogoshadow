1cc4cb99ef6fecf342e8d2943e8a2ba51599650e
suisen.no.ryuu <suisen.no.ryuu@17fd7b7e-20b4-11de-a108-cd2f117ce590>
2010-02-17 22:44:29 +0000

    first steps on DrawInventory

--- trunk/TCC/Scripts/Level1/Inventory/inventoryManager.lua
+++ trunk/TCC/Scripts/Level1/Inventory/inventoryManager.lua
@@ -2,21 +2,23 @@ function start()
 	print("Starting inventory manager")
 end
 
-show = false
+visible = false
 
 function update()
   --Binds the object to use its functions
   --local gameObj = GameObject.ReBinder(parentObject)
   local component = ComponentGUI.GetFrom("OBJ_GUI")
+  
 --  print (component)
   if component:IsButtonPressed("inventoryButton") > 0 then
-    
-    Game.DisplayInventory(true)
+    visible = not visible
 
     print("Apertou")
   	print(component:IsButtonPressed("inventoryButton"))
 
   end
+  Game.DisplayInventory(visible)
+  Game.DrawInventory()
   --local mouseX,mouseY = Game.GetMousePosition()
   --print(mouseX, mouseY)
 end
