f3c37e9be8eb0d0228b06a587148320000e01cd6
rafarlira <rafarlira@17fd7b7e-20b4-11de-a108-cd2f117ce590>
2010-02-23 23:35:15 +0000

    Fix click

--- TCC/Scripts/Common/input.lua
+++ TCC/Scripts/Common/input.lua
@@ -6,32 +6,32 @@ local oldMouseLeftButton = 0
 local oldMouseRightButton = 0
 
 function update()
-  
-  local mouseLeftButton = Game.GetMouseLeft()
-  local mouseRightButton = Game.GetMouseRight()
-  if (((mouseLeftButton == 1) and not (oldMouseLeftButton == mouseLeftButton))
-  or ((mouseRightButton == 1) and not (oldMouseRightButton == mouseRightButton))) then
-  
-    --local mouseX,mouseY = Game.GetMousePosition()
-    --print(mouseX, mouseY)
-    local interact = Game.PickNearestInteract()
-    if (interact) then
-      if (mouseLeftButton == 1) then
-        interact:Inspect()
-      else
-        local selItem = Game.GetSelectedItem()
-        if (selItem == "") then
-          interact:Interact()
+  if (Game.IsInventoryVisible() == 0) then
+    local mouseLeftButton = Game.GetMouseLeft()
+    local mouseRightButton = Game.GetMouseRight()
+    if (((mouseLeftButton == 1) and not (oldMouseLeftButton == mouseLeftButton))
+    or ((mouseRightButton == 1) and not (oldMouseRightButton == mouseRightButton))) then
+    
+      --local mouseX,mouseY = Game.GetMousePosition()
+      --print(mouseX, mouseY)
+      local interact = Game.PickNearestInteract()
+      if (interact) then
+        if (mouseLeftButton == 1) then
+          interact:Inspect()
         else
-          interact:InteractWith(selItem)
+          local selItem = Game.GetSelectedItem()
+          if (selItem == "") then
+            interact:Interact()
+          else
+            interact:InteractWith(selItem)
+          end
         end
       end
     end
+    
+    oldMouseLeftButton = mouseLeftButton;
+    oldMouseRightButton = mouseRightButton;
   end
-  
-  oldMouseLeftButton = mouseLeftButton;
-  oldMouseRightButton = mouseRightButton;
-  
 end
 
 function destroy()
